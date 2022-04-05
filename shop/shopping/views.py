03a72dd7806a48266e5a025cb596d408ddcebe77
Elena Surova <surovaen@gmail.com>
2022-04-06 00:28:59 +1000

    Обновлен shopping/views.py (добавлена функция category, обновлены функции get_category, get_brand)

--- shop/shopping/views.py
+++ shop/shopping/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
-from .models import *
+from .models import CategoryDB, BrandNameDB, GoodsDB
+
+
+def category(request):
+    """тестовая функция отображения страницы категорий и производителей"""
+    categories = CategoryDB.objects.all()
+    brands = BrandNameDB.objects.all()
+    goods = GoodsDB.objects.all()
+    context = {
+        'title': 'Категории товаров',
+        'categories': categories,
+        'brands': brands,
+        'goods': goods,
+    }
+    return render(request, "shopping/category.html", context=context)
 
 
 def shopping(request):
@@ -10,7 +24,14 @@ def shopping(request):
 
 def get_category(request, slug):
     '''тестовая функция просмотра товара по категориям'''
-    return render(request, 'shopping/get_category.html', {'hello': f'Hello, category {slug}!'})
+    goods = GoodsDB.objects.filter(category__slug=slug)
+    categories = CategoryDB.objects.get(slug=slug)
+    context = {
+        'title': categories.title,
+        'goods': goods,
+        'category': categories
+    }
+    return render(request, 'shopping/get_category.html', context=context)
 
 
 def show_image(request, slug):
@@ -20,7 +41,14 @@ def show_image(request, slug):
 
 def get_brand(request, slug):
     '''тестовая функция просмотра товара по производителю'''
-    return render(request, 'shopping/brand.html', {'hello': f'Hello, brand {slug}!'})
+    goods = GoodsDB.objects.filter(brand__slug=slug)
+    brands = BrandNameDB.objects.get(slug=slug)
+    context = {
+        'title': brands.title,
+        'goods': goods,
+        'category': brands
+    }
+    return render(request, 'shopping/brand.html', context=context)
 
 
 def show_good(request, slug):
