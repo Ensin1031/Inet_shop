2180d241845cdcf469d4d88369a9a3cc1f25cfc0
Elena Surova <surovaen@gmail.com>
2022-04-06 00:27:18 +1000

    Обновлен shopping/url.py (добавлен маршрут products)

--- shop/shopping/urls.py
+++ shop/shopping/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', shopping, name='shopping'),
+    path('products/', category, name='products'),
     path('category/<str:slug>/', get_category, name='category'),
     path('show_image/<str:slug>/', show_image, name='show_image'),
     path('brand/<str:slug>/', get_brand, name='brand'),
