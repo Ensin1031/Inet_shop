d5a35b96639df53c2662c4c9e3dc158ff474a960
Elena Surova <94726007+surovaen@users.noreply.github.com>
2022-04-06 19:49:04 +1000

    Update urls.py
    Откат предыдущих изменений

--- shop/shopping/views.py
+++ shop/shopping/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-from .models import CategoryDB, BrandNameDB, GoodsDB
+from .models import *
 
 
-def category(request):
-    """тестовая функция отображения страницы категорий и производителей"""
-    categories = CategoryDB.objects.all()
-    brands = BrandNameDB.objects.all()
-    goods = GoodsDB.objects.all()
-    context = {
-        'title': 'Категории товаров',
-        'categories': categories,
-        'brands': brands,
-        'goods': goods,
-    }
-    return render(request, "shopping/category.html", context=context)
+class ShowGood(DetailView):
+    model = GoodsDB
+    template_name = 'shopping/show_good.html'
+    context_object_name = 'good_item'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['category'] = CategoryDB.objects.get(pk=self.kwargs['category__slug'])
+        context['photos'] = GalleryDB.objects.filter(product=context['object'])
+        context['reviews'] = ReviewsDB.objects.filter(good=context['object'])
+        context['new_price'] = int(context['object'].price * 0.75)
+        return context
+
+
+class ShowAllGoods(ListView):
+    model = GoodsDB
+    template_name = 'shopping/shopping.html'
+    context_object_name = 'goods'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ShowAllGoods, self).get_context_data(**kwargs)
+        context['photos'] = GalleryDB.objects.all()
+        return context
+
+    def get_queryset(self):
+        '''используем, чтобы вывести только те товары, которые помечены, как наличные в магазине'''
+        return GoodsDB.objects.filter(presence=True)
 
 
 def shopping(request):
@@ -24,14 +40,7 @@ def shopping(request):
 
 def get_category(request, slug):
     '''тестовая функция просмотра товара по категориям'''
-    goods = GoodsDB.objects.filter(category__slug=slug)
-    categories = CategoryDB.objects.get(slug=slug)
-    context = {
-        'title': categories.title,
-        'goods': goods,
-        'category': categories
-    }
-    return render(request, 'shopping/get_category.html', context=context)
+    return render(request, 'shopping/get_category.html', {'hello': f'Hello, category {slug}!'})
 
 
 def show_image(request, slug):
@@ -41,16 +50,9 @@ def show_image(request, slug):
 
 def get_brand(request, slug):
     '''тестовая функция просмотра товара по производителю'''
-    goods = GoodsDB.objects.filter(brand__slug=slug)
-    brands = BrandNameDB.objects.get(slug=slug)
-    context = {
-        'title': brands.title,
-        'goods': goods,
-        'category': brands
-    }
-    return render(request, 'shopping/brand.html', context=context)
+    return render(request, 'shopping/brand.html', {'hello': f'Hello, brand {slug}!'})
 
 
-def show_good(request, slug):
-    '''тестовая функция просмотра товара по производителю'''
-    return render(request, 'shopping/good.html', {'hello': f'Hello, good {slug}!'})
+# def show_good(request, slug):
+#     '''тестовая функция просмотра товара по производителю'''
+#     return render(request, 'shopping/show_good.html', {'hello': f'Hello, good {slug}!'})
