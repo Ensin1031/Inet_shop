4085290fb5d697796cb9f23dca73a253042cd61c
Elena Surova <surovaen@gmail.com>
2022-04-07 00:28:23 +1000

    Update main_app/apps.py (verbose_name)

--- shop/main_app/apps.py
+++ shop/main_app/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
-    verbose_name = 'Магазин'
+    verbose_name = 'Главная страница'
