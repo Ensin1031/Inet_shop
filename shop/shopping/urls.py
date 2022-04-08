94eace2b22aaf90d0b458b5ae08307b4b89c7a2a
Ensin1031 <Ensin81@mail.ru>
2022-04-08 21:53:11 +1000

    Приложение shopping поправил модели, установил приложения django-autoslug django-uuslug

--- shop/shopping/urls.py
+++ shop/shopping/urls.py
@@ -5,8 +5,9 @@ from .views import *
 
 urlpatterns = [
     path('', ShowAllGoods.as_view(), name='shopping'),
+    path('good/<str:slug>/', ShowGood.as_view(), name='good'),
+    path('add_review/<str:slug>/', CreateReview.as_view(), name='add_review'),
     path('category/<str:slug>/', get_category, name='category'),
     path('show_image/<str:slug>/', show_image, name='show_image'),
     path('brand/<str:slug>/', get_brand, name='brand'),
-    path('good/<str:slug>/', ShowGood.as_view(), name='good'),
 ]
