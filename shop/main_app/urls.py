ba4f445dc3c765fc37aa284e8f6fa30e50c601aa
Elena Surova <surovaen@gmail.com>
2022-04-11 23:56:31 +1000

    Обновлены пути в main_app/urls.py

--- shop/main_app/urls.py
+++ shop/main_app/urls.py
@@ -1,20 +1,22 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.contrib.flatpages import views
 from .views import *
+
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='index'),
     path('search/', SearchGoodView.as_view(), name='search'),
-    path('blog/', BlogView.as_view(), name='blog'),
-    path('about/', AboutView.as_view(), name='about'),
-    path('contact/', ContactView.as_view(), name='contact'),
-    path('payments/', PaymentView.as_view(), name='payments'),
-    path('delivery/', DeliveryView.as_view(), name='delivery'),
-    path('faqs/', FAQsView.as_view(), name='faqs'),
-    path('support/', SupportView.as_view(), name='support'),
-    path('privacy/', PrivacyView.as_view(), name='privacy'),
-    path('warranty/', WarrantyView.as_view(), name='warranty'),
-    path('conditions/', ConditionsView.as_view(), name='conditions'),
-    path('return/', ReturnView.as_view(), name='return'),
-    path('intellproperty/', IntellPropertyView.as_view(), name='intellproperty'),
+    path('about/', views.flatpage, {'url': '/about/'}, name='about'),
+    path('contact/', views.flatpage, {'url': '/contact/'}, name='contact'),
+    path('payments/', views.flatpage, {'url': '/payments/'}, name='payments'),
+    path('delivery/', views.flatpage, {'url': '/delivery/'}, name='delivery'),
+    path('faqs/', views.flatpage, {'url': '/faqs/'}, name='faqs'),
+    path('support/', views.flatpage, {'url': '/support/'}, name='support'),
+    path('privacy/', views.flatpage, {'url': '/privacy/'}, name='privacy'),
+    path('warranty/', views.flatpage, {'url': '/warranty/'}, name='warranty'),
+    path('conditions/', views.flatpage, {'url': '/conditions/'}, name='conditions'),
+    path('return/', views.flatpage, {'url': '/return/'}, name='return'),
+    path('intellproperty/', views.flatpage, {'url': '/intellproperty/'}, name='intellproperty'),
+    path('promotion_1/', PromotionOneView.as_view(), name='promotion_1'),
+    path('promotion_2/', PromotionTwoView.as_view(), name='promotion_2'),
 ]
